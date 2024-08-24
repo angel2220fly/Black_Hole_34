2eb1bcd4e5c7a0eca86280b4cf04a2adfcf7a046
JURJIUS PACALOVAS <56896464+angel2220fly@users.noreply.github.com>
2024-08-24 16:00:37 +0100

    Add files via upload

--- Black_Hole_34.py
+++ Black_Hole_34.py
@@ -43,11 +43,11 @@ class compression:
         def Count_adds(M1, En, Row1, Row):
 
             Row += 1
+            M=1
 
             if Row == (8192 * 4) - 1:
                 Row = 0
-            if M1 == 0:
-                En -= 1
+    
 
             if En == 3 or M1 == 1:
                 En += 1
@@ -278,7 +278,7 @@ class compression:
 
                                 Find = 0
 
-                                En = 6
+                                En = 3
 
                                 Ci = 1
 
